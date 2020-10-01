6bbc63995681a6f473d921fd00d6d5c32b2aaaa3
David Hernández Uriostegui <53274521+DavidHdezU@users.noreply.github.com>
2020-09-30 23:18:06 -0500

    Añadiendo traductor y comentado

--- Tarea01/src/Menu.py
+++ Tarea01/src/Menu.py
@@ -1,24 +1,24 @@
-import Voz as voice
+from Voz import Voz
 from City import City
 import requests
 import os
 
-
-path=str(os.getcwd()) #Obtiene la ruta relativa en la computadora de trabajo actual
+voice = Voz()
+path = str(os.getcwd()) #Obtiene la ruta relativa en la computadora de trabajo actual
 file = open(path+"/cities.txt", "r")#archivo de lectura
 conjunto = set()
 for x in file:
   conjunto.add(x.rstrip("\n"))
 file.close()
 
-api_adrees = 'http://api.openweathermap.org/data/2.5/weather?appid=1bfd917ba8b375efeea803bf7b9b1ee0&q='
+api_adrees = 'http://api.openweathermap.org/data/2.5/weather?appid=1bfd917ba8b375efeea803bf7b9b1ee0&q=' # Llave del API para obtener info
 
 for city in conjunto:
   url = api_adrees + city
-
+  
   json_data = requests.get(url).json()
 
-  ciudad = City(city,  json_data['main']['temp'], json_data['weather'][0]['description']);
+  ciudad = City(city, json_data['main']['temp'], json_data['weather'][0]['description'])
 
   print(ciudad.imprime_ciudad())
   voice.into_start()
--- Tarea01/src/Voz.py
+++ Tarea01/src/Voz.py
@@ -1,55 +1,60 @@
-"""
- 	Voz 
-	Conversion de Texto a Voz motor del habla del climatrón para el anuncio del clima 
 
-
-"""
 import os
 import random
 from gtts import gTTS
 from playsound import playsound
 #import speech_recognition as sr
-import tiempo as time
+from Tiempo import Tiempo
 
+class Voz:
+	""" Conversion de Texto a Voz motor del habla del climatrón para el anuncio del clima. """
+	def __init__(self):
+		""" Constructor que tiene como atributo el tiempo """
+		self.tiempo = Tiempo()
+		pass
 
-
-
-def say(word):
-		my_word=str(word)
-		tts=gTTS(my_word, lang = 'es-us')
+	def say(self, word):
+		""" Recibe una frase y hace que gtts guarde en un archivo de voz el contenido de la frase y lo reproduzca. """
+		my_word = str(word)
+		tts = gTTS(my_word, lang = 'es-us') # Frase e idioma en el que la voz será
 		tts.save("resources/speaking.mp3")	
 		playsound("resources/speaking.mp3")
 
-def say_slow(word):
-		my_word=str(word)
-		tts=gTTS(my_word, lang = 'es-us', slow = True)
+	def say_slow(self, word):
+		""" Recibe una frase y hace que gtts guarde en un archivo de voz el contenido de la frase y lo reproduzca lentamente."""
+		my_word = str(word)
+		tts = gTTS(my_word, lang = 'es-us', slow = True)
 		tts.save("resources/speaking.mp3")	
 		playsound("resources/speaking.mp3")
 
-def save_audio(word):
-		my_word=str(word)
+	def save_audio(self, word):
+		""" Recibe una frase y hace que gtts guarde en un archivo de voz el contenido de la frase."""
+		my_word = str(word)
 		tts=gTTS(my_word, lang = 'es-us')
 		tts.save("resources/saved.mp3")
 
-def say_saved_audio():
+	def say_saved_audio(self):
+		""" Reproduce el audio del archiv0 saved.mp3 """
 		playsound("resources/saved.mp3")
 
-def into_start():
-	playsound("resources/tones/announcement.mp3")
+	def into_start(self):
+		""" Reproduce el audio del archivp announcement.mp3 """
+		playsound("resources/tones/announcement.mp3")
 
 
-def greet():
-	if time.get_sun() == 'AM':
-		saludo = open("resources/greeting/greeting_morning.txt","r")
-		text=saludo.read().split(',')
-		saludo.close()
-		chosen=str(random.choice(text))
-		say(chosen)
-	if time.get_sun() == 'PM':
-		saludo = open("resources/greeting/greeting_afternoon.txt","r")
-		text=saludo.read().split(',')
-		saludo.close()
-		chosen=str(random.choice(text))
-		say(chosen)
+	def greet(self):
+		""" Lee un archivo dependiendo si la hora es AM o PM y hace que el asistente de voz lo lea"""
+		if self.tiempo.get_sun() == 'AM':
+			saludo = open("resources/greeting/greeting_morning.txt","r")
+			text=saludo.read().split(',')
+			saludo.close()
+			chosen=str(random.choice(text)) # Selecciona saludo random
+			self.say(chosen) # El asistente de voz lee
+		if self.tiempo.get_sun() == 'PM':
+			saludo = open("resources/greeting/greeting_afternoon.txt","r")
+			text=saludo.read().split(',')
+			saludo.close()
+			chosen=str(random.choice(text))  #Selecciona saludo random
+			self.say(chosen) # El asistente de voz lee
 
 
